762328d46c3be85ed44aa2b3ad028e17ca0bd835
ani-5 <Awnish321@gmail.com>
2023-07-01 17:57:59 +0530

    ist

--- app/src/main/java/com/rachnasagar/adapter/GridViewAdapterBooksDownload.java
+++ app/src/main/java/com/rachnasagar/adapter/GridViewAdapterBooksDownload.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ import com.rachnasagar.Common.AllStaticMethod;
 import com.rachnasagar.R;
 import com.rachnasagar.activity.Book_Download_Page;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -27,17 +29,18 @@ public class GridViewAdapterBooksDownload extends ArrayAdapter<VinSetterGetter> 
     public static String Value;
     String zipFile;
     String unzipLocation;
-
+    String file_details;
+    String file_activities;
+    String name;
+    File ebook_directory;
 
     public GridViewAdapterBooksDownload(@NonNull Context context, ArrayList<VinSetterGetter> courseModelArrayList) {
         super(context, 0, courseModelArrayList);
     }
-
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
     {
-
         View grid = convertView;
         if (grid == null) {
             grid = LayoutInflater.from(getContext()).inflate(R.layout.download_bookdetails, parent, false);
@@ -55,6 +58,18 @@ public class GridViewAdapterBooksDownload extends ArrayAdapter<VinSetterGetter> 
 
         txt_Book_Title.setText(vimSetter.getBookName());
         txt_Book_Type.setText(vimSetter.getBookType());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ebook_directory = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+            Log.d("awa0", "" + ebook_directory);
+        } else {
+            ebook_directory = Environment.getExternalStorageDirectory();
+            Log.d("awa00", "" + ebook_directory);
+        }
+
+//        unzipLocation = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) + "/.Rachna/" + Book_Type + "/";
+//        file_details = new File(ebook_directory, "/.Rachna/" + Book_Type + "/" + Book_Zip_Title + ".pdf");
+//        file_activities = new File(ebook_directory, "/.Rachna/" + Book_Type + "/" + "." + Book_Zip_Title);
+
         if (Objects.equals(vimSetter.getProduct_Subscription_Type(), "Paid")) {
             ebook_text_book_type.setText(" ( Paid )");
         }
@@ -69,25 +84,26 @@ public class GridViewAdapterBooksDownload extends ArrayAdapter<VinSetterGetter> 
                 getContext().startActivity(intent);
             }
         });
-        img_download.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Value = null;
-                Value = "Book_Zip_Title" + ".zip";
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    zipFile = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) + "/" + "Book_Zip_Title" + ".zip";
-                    unzipLocation = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) + "/.Rachna/" + "Book_Type" + "/";
-                    Toast.makeText(getContext(), zipFile, Toast.LENGTH_SHORT).show();
-                    AllStaticMethod.downloadZip(getContext(),"zip","zipt",zipFile,unzipLocation);
 
-                } else {
-                    zipFile = Environment.getExternalStorageDirectory() + "/" + "Book_Zip_Title" + ".zip";
-                    unzipLocation = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) + "/.Rachna/" + "Book_Type" + "/";
-                    Toast.makeText(getContext(), zipFile, Toast.LENGTH_SHORT).show();
-                    AllStaticMethod.downloadZip(getContext(),"zip","zipt",zipFile,unzipLocation);
-                }
-            }
-        });
+//        img_download.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Value = null;
+//                Value = "Book_Zip_Title" + ".zip";
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//                    zipFile = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) + "/" + "Book_Zip_Title" + ".zip";
+//                    unzipLocation = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) + "/.Rachna/" + "Book_Type" + "/";
+//                    Toast.makeText(getContext(), zipFile, Toast.LENGTH_SHORT).show();
+//                    AllStaticMethod.downloadZip(getContext(),"zip","zipt",zipFile,unzipLocation);
+//
+//                } else {
+//                    zipFile = Environment.getExternalStorageDirectory() + "/" + "Book_Zip_Title" + ".zip";
+//                    unzipLocation = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) + "/.Rachna/" + "Book_Type" + "/";
+//                    Toast.makeText(getContext(), zipFile, Toast.LENGTH_SHORT).show();
+//                    AllStaticMethod.downloadZip(getContext(),"zip","zipt",zipFile,unzipLocation);
+//                }
+//            }
+//        });
 
         return grid;
     }
